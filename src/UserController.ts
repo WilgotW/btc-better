0ba12899740b89e78e7b9c64dee32f5b369db0cf
Wilgot Wardell <wilgotwardell@hotmail.com>
2023-10-04 12:36:08 +0200

    -

--- src/UserController.ts
+++ src/UserController.ts
@@ -1,4 +1,7 @@
 import express, { Router } from "express";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { error } from "console";
 
 export default class UserController {
     constructor(){
@@ -9,9 +12,29 @@ export default class UserController {
         const router: Router = express.Router();
 
         router.post("/register", (req, res) => {
-            
+            const email = req.body.email;
+            const password = req.body.password;
+
+            const user ={
+                email,
+                passwordHash : bcrypt.hash(password, 10)
+            }
+
+
         })
         router.post("login", (req, res) => {
+            const email = req.body.email;
+            const password = req.body.password;
+            // prisma get user
+
+            // if(!bcrypt.compare(password, user.passwrodHash)){
+                throw error("incident")
+            // }
+
+            const token = jwt.sign({}, "secret")
+
+            return token;
+
 
         })
 
--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,13 @@ import cheerio from "cheerio";
 
 const app = express();
 
-console.log("hello");
 
 app.get("/", (req, res: express.Response) => {
   res.send("home");
 });
 
-async function scrapePrice() {
-  const url = "https://www.bullionbypost.eu/gold-price/live-gold-price/";
-  try {
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data);
-
-    const priceElement = $(
-      'span[name="current_price_field"][data-currency="default"]'
-    );
-    const priceText = priceElement.text();
-
-    console.log("Scraped Price:", priceText.trim());
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
 //routes
 const userController = new UserController();
 app.use(userController.routes());
 
-app.listen(4000, () => scrapePrice());
+app.listen(4000, () => console.log("live"));
