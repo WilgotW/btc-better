0336af6552bf5dfc6f170af12e69b92eae4128f5
Wilgot <wilgotwardell@hotmail.com>
2023-10-26 17:00:34 +0200

    betting system working

--- src/controllers/betController.ts
+++ src/controllers/betController.ts
@@ -7,9 +7,11 @@ export default class BetController {
   async newBet(betData: Bet) {
     try {
       const betStartTimeStamp = Math.floor(betData.startDate.getTime() / 1000);
-      const betEndTimeStamp = Math.floor(betData.endDate.getTime() / 1000);
+      const betEndTimeStamp = Math.floor(
+        (betData.endDate.getTime() + 60000) / 1000
+      );
 
-      const newBet = prisma.bets.create({
+      const newBet = await prisma.bets.create({
         data: {
           userid: betData.userId,
           ticker: betData.ticker,
@@ -22,5 +24,33 @@ export default class BetController {
     } catch (err) {
       throw new Error("error");
     }
+  }
+
+  async allBets(userId: number) {
+    const userBets = await prisma.bets.findMany({
+      where: {
+        userid: userId,
+      },
+    });
+    return userBets;
+  }
+
+  async checkBetsEnd(userId: number) {
+    const userBets = await prisma.bets.findMany({
+      where: {
+        userid: userId,
+      },
+    });
+
+    const date = new Date();
+    const currentTimestamp = Math.floor(date.getTime() / 1000);
+
+    const betsDone = userBets.filter((bet) => {
+      //check if bet is done
+      if (bet.enddate <= currentTimestamp) {
+        return bet;
+      }
+    });
+    return betsDone;
   }
 }
--- src/routes/betRoutes.ts
+++ src/routes/betRoutes.ts
@@ -1,33 +1,62 @@
 import { Router, Response, Request } from "express";
-import Bet from "../models/Bet";
 import BetController from "../controllers/betController";
 
 const betRouter = Router();
 const betController = new BetController();
 
 betRouter.post("/create", async (req: Request, res: Response) => {
-  //create new bet
   const data = req.body;
-  if (data) {
-    try {
-      const betStart = new Date();
-      const betEnd = new Date();
+  if (!data) return;
+  try {
+    const betStart = new Date();
+    const betEnd = new Date();
 
-      const newBet = await betController.newBet({
-        userId: data.userId,
-        ticker: data.ticker,
-        startDate: betStart,
-        endDate: betEnd,
-        amount: data.amount,
-      });
-      if (newBet) {
-        res.json(newBet);
-      } else {
-        res.status(400).send("couldn't create bet");
-      }
-    } catch (err) {
-      throw new Error("error accessing data");
+    const newBet = await betController.newBet({
+      userId: data.userId,
+      ticker: data.ticker,
+      startDate: betStart,
+      endDate: betEnd,
+      amount: data.amount,
+    });
+    if (newBet) {
+      res.json(newBet);
+    } else {
+      res.status(400).send("couldn't create bet");
     }
+  } catch (err) {
+    throw new Error("error accessing bet data");
+  }
+});
+
+betRouter.post("/get-all", async (req: Request, res: Response) => {
+  const id: number = req.body.userId;
+  if (!id) res.status(400).send("no id found");
+
+  try {
+    const allBets = await betController.allBets(id);
+    if (allBets.length) {
+      res.json(allBets);
+    } else {
+      res.send("no bets");
+    }
+  } catch (err) {
+    throw new Error("error accessing bet data");
+  }
+});
+
+betRouter.post("/check-bets", async (req: Request, res: Response) => {
+  const id: number = req.body.userId;
+  if (!id) res.status(400).send("no id found");
+
+  try {
+    const doneBets = await betController.checkBetsEnd(id);
+    if (doneBets.length) {
+      res.json(doneBets);
+    } else {
+      res.send("no bets have ended");
+    }
+  } catch (err) {
+    throw new Error("error accessing bet data");
   }
 });
 
